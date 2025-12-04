f066cc77c1e4de0faa1866a8dbc68c15414136ab
llkessioll <kessio_baixista@hotmail.com>
2025-12-03 23:08:14 -0300

    feat: Iniciando o projeto com spring initializer

---
+++ Sistema-de-Gerenciamento-de-Tarefas-To-Do-List/src/main/java/com/example/Sistema_de_Gerenciamento_de_Tarefas_To_Do_List/SistemaDeGerenciamentoDeTarefasToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sistema_de_Gerenciamento_de_Tarefas_To_Do_List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGerenciamentoDeTarefasToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGerenciamentoDeTarefasToDoListApplication.class, args);
+	}
+
+}
